3a55f9cf8a6081345daa236273d7e1f78cf22f33
tacomeet <takumihara1226@gmail.com>
2022-03-24 21:01:44 +0900

    feat: add func for EAN13

--- test/checkdigit/gtin_test.exs
+++ test/checkdigit/gtin_test.exs
@@ -2,8 +2,10 @@ defmodule CheckdigitJAN13Test do
   use ExUnit.Case
   doctest Checkdigit.JAN13
   doctest Checkdigit.EAN8
+  doctest Checkdigit.EAN13
   alias Checkdigit.JAN13
   alias Checkdigit.EAN8
+  alias Checkdigit.EAN13
 
   test "JAN13.generate with regular patterns" do
     assert JAN13.generate("456995111617") == {:ok, 9}
@@ -34,4 +36,11 @@ defmodule CheckdigitJAN13Test do
     assert EAN8.verify("96385074") == true
   end
 
+  test "EAN13.generate with regular patterns" do
+    assert EAN13.generate("590123412345") == {:ok, 7}
+  end
+
+  test "EAN13.verify with regular patterns" do
+    assert EAN13.verify("5901234123457") == true
+  end
 end
