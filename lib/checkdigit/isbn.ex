1b651ac11c915078db501eb44f440a3c1e0b1ce0
tacomeet <takumihara1226@gmail.com>
2022-03-28 20:39:14 +0900

    feat: add ISBN13

--- lib/checkdigit/isbn.ex
+++ lib/checkdigit/isbn.ex
@@ -42,3 +42,46 @@ defmodule Checkdigit.ISBN10 do
     end
   end
 end
+
+defmodule Checkdigit.ISBN13 do
+  def verify(code) do
+    if String.length(code) != 13 do
+      false
+    else
+      try do
+        case generate(String.slice(code, 0..-2)) do
+          {:ok, generated} -> generated == String.last(code) |> String.to_integer()
+          {:error, _} -> false
+        end
+      rescue
+        _ in ArgumentError -> false
+      end
+    end
+  end
+
+  def generate(seed) do
+    if String.length(seed) != 12 do
+      {:error, "checkdigit: length of seed is not appropriate"}
+    else
+      try do
+        {
+          :ok,
+          seed
+          |> String.codepoints()
+          |> Enum.map(&String.to_integer/1)
+          |> Enum.with_index()
+          |> Enum.reduce(0, fn
+            {v, i}, acc when rem(i, 2) == 0 -> acc + v
+            {v, _i}, acc -> acc + v * 3
+          end)
+          |> rem(10)
+          |> Kernel.-(10)
+          |> Kernel.*(-1)
+          |> rem(10)
+        }
+      rescue
+        _ in ArgumentError -> {:error, "checkdigit: seed contains non-number character"}
+      end
+    end
+  end
+end
--- test/checkdigit/isbn_test.exs
+++ test/checkdigit/isbn_test.exs
@@ -1,33 +1,63 @@
 defmodule CheckdigitISBNTest do
   use ExUnit.Case
   doctest Checkdigit.ISBN10
+  doctest Checkdigit.ISBN13
 
-  import Checkdigit.ISBN10, only: [generate: 1, verify: 1]
+  alias Checkdigit.ISBN10
+  alias Checkdigit.ISBN13
 
-  test "generate: regular" do
-    assert generate("002651562") == {:ok, 8}
-    assert generate("007231592") == {:ok, 10}
-    assert generate("155860832") == {:ok, 10}
+  test "ISBN10.generate: regular" do
+    assert ISBN10.generate("002651562") == {:ok, 8}
+    assert ISBN10.generate("007231592") == {:ok, 10}
+    assert ISBN10.generate("155860832") == {:ok, 10}
   end
 
-  test "generate: irregular" do
-    assert generate("9780002715096") == {:error, "checkdigit: length of seed is not appropriate"}
-    assert generate("15586") == {:error, "checkdigit: length of seed is not appropriate"}
-    assert generate("15586083X") == {:error, "checkdigit: seed contains non-number character"}
-    assert generate("aaaaaaaaa") == {:error, "checkdigit: seed contains non-number character"}
+  test "ISBN10.generate: irregular" do
+    assert ISBN10.generate("9780002715096") == {:error, "checkdigit: length of seed is not appropriate"}
+    assert ISBN10.generate("15586") == {:error, "checkdigit: length of seed is not appropriate"}
+    assert ISBN10.generate("15586083X") == {:error, "checkdigit: seed contains non-number character"}
+    assert ISBN10.generate("aaaaaaaaa") == {:error, "checkdigit: seed contains non-number character"}
   end
 
-  test "verify: regular" do
-    assert verify("0026515628") == true
-    assert verify("007231592X") == true
-    assert verify("155860832X") == true
+  test "ISBN10.verify: regular" do
+    assert ISBN10.verify("0026515628") == true
+    assert ISBN10.verify("007231592X") == true
+    assert ISBN10.verify("155860832X") == true
   end
 
-  test "verify: irregular" do
-    assert verify("155860831X") == false
-    assert verify("9780002715096") == false
-    assert verify("155860831") == false
-    assert verify("") == false
-    assert verify("aaaaaaaaaa") == false
+  test "ISBN10.verify: irregular" do
+    assert ISBN10.verify("155860831X") == false
+    assert ISBN10.verify("9780002715096") == false
+    assert ISBN10.verify("155860831") == false
+    assert ISBN10.verify("") == false
+    assert ISBN10.verify("aaaaaaaaaa") == false
+  end
+
+  test "ISBN13.generate: regular" do
+    assert ISBN13.generate("978000271217") == {:ok, 0}
+    assert ISBN13.generate("978000271330") == {:ok, 6}
+    assert ISBN13.generate("978000271363") == {:ok, 4}
+    assert ISBN13.generate("978000271236") == {:ok, 1}
+  end
+
+  test "ISBN13.generate: irregular" do
+    assert ISBN13.generate("") == {:error, "checkdigit: length of seed is not appropriate"}
+    assert ISBN13.generate("0026515628") == {:error, "checkdigit: length of seed is not appropriate"}
+    assert ISBN13.generate("15586083211X") == {:error, "checkdigit: seed contains non-number character"}
+    assert ISBN13.generate("aaaaaaaaaaaa") == {:error, "checkdigit: seed contains non-number character"}
+  end
+
+  test "ISBN13.verify: regular" do
+    assert ISBN13.verify("9780002712095") == true
+    assert ISBN13.verify("9780002715096") == true
+    assert ISBN13.verify("9780002713306") == true
+  end
+
+  test "ISBN13.verify: irregular" do
+    assert ISBN13.verify("155860831X") == false
+    assert ISBN13.verify("9780002712520") == false
+    assert ISBN13.verify("9780002712709") == false
+    assert ISBN13.verify("") == false
+    assert ISBN13.verify("aaaaaaaaaaaaa") == false
   end
 end
